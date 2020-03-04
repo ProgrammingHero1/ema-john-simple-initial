4a35ba2b75ffa6c6046e1518296211151e648f78
jhankarpHero <jhankar.mahbub1@gmail.com>
2020-03-03 21:25:42 -0800

    auth testing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import logo from '../../images/logo.png';
 import './Header.css';
+import { useAuth } from "../Auth/use-auth";
+import {
+    Link
+  } from "react-router-dom";
 
 const Header = () => {
+    const auth = useAuth();
+    console.log(auth);
     return (
         <div className="header">
             <img src={logo} alt=""/>
@@ -10,6 +16,13 @@ const Header = () => {
                 <a href="/shop">Shop</a>
                 <a href="/review">Order Review</a>
                 <a href="/inventory">Manage Inventory</a>
+                {auth && auth.user && <span style={{color:'white'}}>{auth.user.email}</span>}
+                {
+                    auth && !auth.user ? <button onClick={()=>auth.signinWithGoogle()}>Sign in</button> 
+                    : <button onClick={() => auth.signout()}>Sign Out</button>
+                }
+                
+                
             </nav>
         </div>
     );
