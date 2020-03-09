4bcc71cdce836bc6e0825a05aa7fea842c0ee62f
khanmd50 <md.khan@nielsen.com>
2020-03-09 08:01:57 -0700

    getting ready for auth integration

--- src/App.js
+++ src/App.js
@@ -12,37 +12,12 @@ import Review from './components/Review/Review';
 import Inventory from './components/Inventory/Inventory';
 import NotFound from './components/NotFound/NotFound';
 import ProductDetail from './components/ProductDetail/ProductDetail';
-import withFirebaseAuth from 'react-with-firebase-auth'
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
-import firebaseConfig from './firebaseConfig';
-import { ProvideAuth } from "./components/Auth/use-auth";
-import Login from './components/Auth/Login';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-//const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 function App(props) {
-  // const {
-  //   user,
-  //   signOut,
-  //   signInWithGoogle,
-  // } = props;
   return (
     <div>
-      <ProvideAuth>
       <Header></Header>
-      {/* {
-            user
-              ? <p>Hello, {user.displayName}</p>
-              : <p>Please sign in.</p>
-          }
-
-          {
-            user
-              ? <button onClick={signOut}>Sign out</button>
-              : <button onClick={signInWithGoogle}>Sign in with Google</button>
-          } */}
       <Router>
         <Switch>
           <Route path="/shop">
@@ -60,28 +35,13 @@ function App(props) {
           <Route path="/product/:productKey">
             <ProductDetail></ProductDetail>
           </Route>
-          <Route path="/login">
-            <Login></Login>
-          </Route>
           <Route path="*">
             <NotFound></NotFound>
           </Route>
         </Switch>
       </Router>
-      </ProvideAuth>
     </div>
   );
 }
-
-// const firebaseAppAuth = firebaseApp.auth();
-
-// const providers = {
-//   googleProvider: new firebase.auth.GoogleAuthProvider(),
-// };
-
-// export default withFirebaseAuth({
-//   providers,
-//   firebaseAppAuth,
-// })(App);
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import logo from '../../images/logo.png';
 import './Header.css';
-import { useAuth } from "../Auth/use-auth";
-import {
-    Link
-  } from "react-router-dom";
 
 const Header = () => {
-    const auth = useAuth();
-    console.log(auth);
     return (
         <div className="header">
             <img src={logo} alt=""/>
@@ -16,13 +10,6 @@ const Header = () => {
                 <a href="/shop">Shop</a>
                 <a href="/review">Order Review</a>
                 <a href="/inventory">Manage Inventory</a>
-                {auth && auth.user && <span style={{color:'white'}}>{auth.user.email}</span>}
-                {
-                    auth && !auth.user ? <button onClick={()=>auth.signinWithGoogle()}>Sign in</button> 
-                    : <button onClick={() => auth.signout()}>Sign Out</button>
-                }
-                
-                
             </nav>
         </div>
     );
