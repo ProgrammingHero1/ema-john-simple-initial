2120bdfc8ebef4cedf35cbd1e4752ee9e0c6d42c
khanmd50 <md.khan@nielsen.com>
2020-03-10 10:53:01 -0700

    installed react hook form

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Shipment = () => {
+    const { register, handleSubmit, watch, errors } = useForm();
+    const onSubmit = data => { console.log(data) }
+  
+    console.log(watch('example')) // watch input value by passing the name of it
+  
     return (
-        <div>
-            <h1>This is Shipment Component</h1>
-        </div>
-    );
+      <form onSubmit={handleSubmit(onSubmit)}>
+      {/* register your input into the hook by invoking the "register" function */}
+        <input name="example" defaultValue="test" ref={register} />
+        
+        {/* include validation with required or other standard HTML validation rules */}
+        <input name="exampleRequired" ref={register({ required: true })} />
+        {/* errors will return when field validation fails  */}
+        {errors.exampleRequired && <span>This field is required</span>}
+        
+        <input type="submit" />
+      </form>
+    )
 };
 
 export default Shipment;
